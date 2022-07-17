a2ec66b60be2322ef7d18b6bd025fb1964865546
JoseAre <A01209817@tec.mx>
2022-07-16 20:53:49 -0500

    Laboratorio completo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-// 1) Reemplaza estas variables por useState, puedes utilizar los valores iniciales que ya traen las variables
-  let temperatureColor = 'cold'
-  let temperatureValue = '10'
-  
-// 2) Agregar las funciones para manejar los eventos de Click a los botones para que 
-//     cuando se presionen se incremente o decremente el valor de la temperatura. 
+const App = () => {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
 
-// 3) Agrega una lógica para que al momento de que el valor sea >= 20 grados, la variable 'temperatureColor' cambie a 'hot'
-//    De igual manera si la temperatura baja a <20 grados, la variable cambie a 'cold'
+  const increaseTemperature = () => {
+    if (temperatureValue === 30) return;
 
-// Para fines prácticos, agrega una regla que evite que los valores suban arriba de 30. Es decir al llegar a 30, no se podrá incrementar más.
-// De igual manera al llegar los valores a 0 no se podrá decrementas más.
+    const newTemperature = temperatureValue + 1;
+
+    setTemperatureValue(newTemperature);
+    if (newTemperature >= 20) {
+      setTemperatureColor("hot");
+    }
+  };
+
+  const decreaseTemperature = () => {
+    if (temperatureValue === 0) return;
+
+    const newTemperature = temperatureValue - 1;
+    setTemperatureValue(newTemperature);
+    if (newTemperature < 20) {
+      setTemperatureColor("cold");
+    }
+  };
 
   return (
     <div className="app-container">
@@ -23,11 +34,11 @@ function App() {
         </div>
       </div>
       <div className="button-container">
-        <button >+</button>
-        <button >-</button>
+        <button onMouseDown={increaseTemperature}>+</button>
+        <button onClick={decreaseTemperature}>-</button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
